6dc19c00947da3abfeea1bb5e181c9764354f7e1
UniverAlex <UniverAlex@ya.ru>
2018-07-18 11:16:20 +0300

    Initial commit

---
+++ src/main/java/ru/univeralex/reactandspringdatarestdemo/ReactAndSpringDataRestDemoApplication.java
@@ -0,0 +1,12 @@
+package ru.univeralex.reactandspringdatarestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringDataRestDemoApplication.class, args);
+	}
+}
