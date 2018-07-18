595186a59f1877a97ab036fbc6b1425b5f8936c4
UniverAlex <UniverAlex@ya.ru>
2018-07-18 12:59:16 +0300

    if applied this commit will set up custom UI controller

---
+++ src/main/java/ru/univeralex/react_and_spring_data_rest_demo/payroll/HomeController.java
@@ -0,0 +1,14 @@
+package ru.univeralex.react_and_spring_data_rest_demo.payroll;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+}
